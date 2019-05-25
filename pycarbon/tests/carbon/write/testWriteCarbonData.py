c4ddfa90ca59067abced70a6a4c1a6d75796c66e
xubo245 <xubo29@huawei.com>
2019-05-25 18:23:33 +0800

    support write image file by base 64 encode binary

--- pycarbon/tests/carbon/write/testWriteCarbonData.py
+++ pycarbon/tests/carbon/write/testWriteCarbonData.py
@@ -11,16 +11,15 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import os
 
 from pycarbon.pysdk.CarbonReader import CarbonReader
 import time
 
 from pycarbon.pysdk.CarbonWriter import CarbonWriter
-from pycarbon.pysdk.SDKUtil import SDKUtil
 from pycarbon.tests import DEFAULT_CARBONSDK_PATH
 
 import unittest
+import base64
 
 
 class WriteCarbonTest(unittest.TestCase):
@@ -81,7 +80,69 @@ class WriteCarbonTest(unittest.TestCase):
     print("all time is " + str(end - start))
     print("Finish")
 
-  def test_2_run_write_carbon_binary(self):
+  def test_2_run_write_carbon_binary_base64_encode(self):
+
+    start = time.time()
+    jsonSchema = "[{stringField:string},{shortField:short},{intField:int},{binaryField:binary}]";
+    path = "/tmp/data/writeCarbon" + str(time.time())
+    jpg_path = "../../data/image/carbondatalogo.jpg"
+    print(path)
+    writer = CarbonWriter() \
+      .builder() \
+      .outputPath(path) \
+      .withCsvInput(jsonSchema) \
+      .writtenBy("pycarbon") \
+      .build()
+
+    with open(jpg_path, mode='rb+') as file_object:
+      content = file_object.read()
+
+    for i in range(0, 10):
+      from jnius import autoclass
+
+      arrayListClass = autoclass("java.util.ArrayList");
+      list = arrayListClass()
+      list.add("pycarbon")
+      list.add(str(i))
+      list.add(str(i * 10))
+      list.add(base64.b64encode(content))
+
+      writer.write(list.toArray())
+    writer.close()
+
+    reader = CarbonReader() \
+      .builder() \
+      .withFolder(path) \
+      .withBatch(1000) \
+      .build()
+
+    i = 0
+    while reader.hasNext():
+      rows = reader.readNextBatchRow()
+      for row in rows:
+        i = i + 1
+        print()
+        if 1 == i % 10:
+          print(str(i) + " to " + str(i + 9) + ":")
+        for column in row:
+          print(column, end="\t")
+          from jnius.jnius import ByteArray
+          if 1 == i and isinstance(column, ByteArray) and len(column) > 1000:
+            with open(path + "/image.jpg", 'wb+') as file_object:
+              file_object.write(base64.b64decode(column.tostring()))
+
+    print()
+    print("number of rows by read: " + str(i))
+    assert 10 == i
+    reader.close()
+
+    end = time.time()
+    print("all time is " + str(end - start))
+    print("Finish")
+
+  # TODO: to be supported
+  @unittest.skip
+  def skiptest_3_run_write_carbon_binary(self):
 
     start = time.time()
     jsonSchema = "[{stringField:string},{shortField:short},{intField:int},{binaryField:binary}]";
@@ -99,20 +160,17 @@ class WriteCarbonTest(unittest.TestCase):
     with open(jpg_path, mode='rb+') as file_object:
       content = file_object.read()
 
-    bytesdata = bytes(content)
-    sdkUtil = SDKUtil()
     for i in range(0, 10):
       from jnius import autoclass
+
       arrayListClass = autoclass("java.util.ArrayList");
       list = arrayListClass()
       list.add("pycarbon")
       list.add(str(i))
       list.add(str(i * 10))
-      jppg_bytes = sdkUtil.SDKUtilClass.readBinary(jpg_path)
-      list.add(bytearray(jppg_bytes))
-      list.add(jppg_bytes)
-      list.add(jppg_bytes)
-      writer.write(list.toArray())
+      list.add(content)
+
+      writer.write(list)
     writer.close()
 
     reader = CarbonReader() \
@@ -122,7 +180,7 @@ class WriteCarbonTest(unittest.TestCase):
       .build()
 
     i = 0
-    while (reader.hasNext()):
+    while reader.hasNext():
       rows = reader.readNextBatchRow()
       for row in rows:
         i = i + 1
@@ -133,8 +191,8 @@ class WriteCarbonTest(unittest.TestCase):
           print(column, end="\t")
           from jnius.jnius import ByteArray
           if 1 == i and isinstance(column, ByteArray) and len(column) > 1000:
-            with open(path + "/image.jpg", 'w+', encoding="ISO-8859-1") as file_object:
-              file_object.write(str(column.tostring(), 'utf-8'))
+            with open(path + "/image.jpg", 'wb+') as file_object:
+              file_object.write(column.tostring())
 
     print()
     print("number of rows by read: " + str(i))
@@ -145,8 +203,8 @@ class WriteCarbonTest(unittest.TestCase):
     print("all time is " + str(end - start))
     print("Finish")
 
-  def test_3_run_write_image(self):
-    path = "/Users/xubo/Desktop/xubo/git/pycarbon/pycarbon/tests/data/image/carbondatalogo.jpg"
+  def test_4_run_write_image(self):
+    path = "../../data/image/carbondatalogo.jpg"
     a = 2 + 1
     print(a)
     from skimage import io
@@ -164,3 +222,221 @@ class WriteCarbonTest(unittest.TestCase):
       bytesdata = bytes(content2, encoding="ISO-8859-1")
       print(len(content2))
       print(len(bytesdata))
+
+  def test_5_run_write_carbon_binary_base64_encode_many_files(self):
+
+    start = time.time()
+    jsonSchema = "[{stringField:string},{shortField:short},{intField:int},{binaryField:binary},{txtField:string}]";
+    path = "/tmp/data/writeCarbon" + str(time.time())
+    jpg_path = "../../data/image/flowers"
+
+    from jnius import autoclass
+
+    sdkUtilClass = autoclass("org.apache.carbondata.sdk.file.utils.SDKUtil");
+    jpg_files = sdkUtilClass.listFiles(jpg_path, '.jpg')
+    print(path)
+
+    writer = CarbonWriter() \
+      .builder() \
+      .outputPath(path) \
+      .withCsvInput(jsonSchema) \
+      .writtenBy("pycarbon") \
+      .build()
+    for i in range(0, jpg_files.size()):
+      jpg_path = jpg_files.get(i)
+      with open(jpg_path, mode='rb+') as file_object:
+        content = file_object.read()
+
+      with open(str(jpg_path).replace('.jpg', '.txt'), mode='r+') as file_object:
+        txt = file_object.read()
+
+      from jnius import autoclass
+
+      arrayListClass = autoclass("java.util.ArrayList");
+      list = arrayListClass()
+      list.add("pycarbon")
+      list.add(str(i))
+      list.add(str(i * 10))
+      list.add(base64.b64encode(content))
+      list.add(txt)
+
+      writer.write(list.toArray())
+
+    writer.close()
+
+    reader = CarbonReader() \
+      .builder() \
+      .withFolder(path) \
+      .withBatch(1000) \
+      .build()
+
+    i = 0
+    while reader.hasNext():
+      rows = reader.readNextBatchRow()
+
+      for row in rows:
+        i = i + 1
+        print()
+        if 1 == i % 10:
+          print(str(i) + " to " + str(i + 9) + ":")
+        for column in row:
+          print(column, end="\t")
+          from jnius.jnius import ByteArray
+          if isinstance(column, ByteArray) and len(column) > 1000:
+            with open(path + "/image" + str(i) + ".jpg", 'wb+') as file_object:
+              file_object.write(base64.b64decode(column.tostring()))
+
+    print()
+    print("number of rows by read: " + str(i))
+    assert 3 == i
+    reader.close()
+
+    end = time.time()
+    print("all time is " + str(end - start))
+    print("Finish")
+
+  def test_6_run_write_carbon_binary_base64_encode_voc(self):
+
+    start = time.time()
+    jsonSchema = "[{stringField:string},{shortField:short},{intField:int},{binaryField:binary},{txtField:string}]";
+    path = "/tmp/data/writeCarbon" + str(time.time())
+    jpg_path = "../../data/image/voc"
+
+    from jnius import autoclass
+
+    sdkUtilClass = autoclass("org.apache.carbondata.sdk.file.utils.SDKUtil");
+    jpg_files = sdkUtilClass.listFiles(jpg_path, '.jpg')
+    print(path)
+
+    writer = CarbonWriter() \
+      .builder() \
+      .outputPath(path) \
+      .withCsvInput(jsonSchema) \
+      .writtenBy("pycarbon") \
+      .build()
+    for i in range(0, jpg_files.size()):
+      jpg_path = jpg_files.get(i)
+      with open(jpg_path, mode='rb+') as file_object:
+        content = file_object.read()
+
+      with open(str(jpg_path).replace('.jpg', '.xml'), mode='r+') as file_object:
+        txt = file_object.read()
+
+      from jnius import autoclass
+
+      arrayListClass = autoclass("java.util.ArrayList");
+      list = arrayListClass()
+      list.add("pycarbon")
+      list.add(str(i))
+      list.add(str(i * 10))
+      list.add(base64.b64encode(content))
+      list.add(txt)
+
+      writer.write(list.toArray())
+
+    writer.close()
+
+    reader = CarbonReader() \
+      .builder() \
+      .withFolder(path) \
+      .withBatch(1000) \
+      .build()
+
+    i = 0
+    while reader.hasNext():
+      rows = reader.readNextBatchRow()
+
+      for row in rows:
+        i = i + 1
+        print()
+        if 1 == i % 10:
+          print(str(i) + " to " + str(i + 9) + ":")
+        for column in row:
+          print(column, end="\t")
+          from jnius.jnius import ByteArray
+          if isinstance(column, ByteArray) and len(column) > 1000:
+            with open(path + "/image" + str(i) + ".jpg", 'wb+') as file_object:
+              file_object.write(base64.b64decode(column.tostring()))
+
+    print()
+    print("number of rows by read: " + str(i))
+    assert 5 == i
+    reader.close()
+
+    end = time.time()
+    print("all time is " + str(end - start))
+    print("Finish")
+
+  def test_7_run_write_carbon_binary_base64_encode_vocForSegmentationClass(self):
+
+    start = time.time()
+    jsonSchema = "[{stringField:string},{shortField:short},{intField:int},{binaryField:binary},{segField:binary}]";
+    path = "/tmp/data/writeCarbon" + str(time.time())
+    jpg_path = "../../data/image/vocForSegmentationClass"
+
+    from jnius import autoclass
+
+    sdkUtilClass = autoclass("org.apache.carbondata.sdk.file.utils.SDKUtil");
+    jpg_files = sdkUtilClass.listFiles(jpg_path, '.jpg')
+    print(path)
+
+    writer = CarbonWriter() \
+      .builder() \
+      .outputPath(path) \
+      .withCsvInput(jsonSchema) \
+      .writtenBy("pycarbon") \
+      .build()
+    for i in range(0, jpg_files.size()):
+      jpg_path = jpg_files.get(i)
+      with open(jpg_path, mode='rb+') as file_object:
+        content = file_object.read()
+
+      with open(str(jpg_path).replace('.jpg', '.png'), mode='rb+') as file_object:
+        png_data = file_object.read()
+
+      from jnius import autoclass
+
+      arrayListClass = autoclass("java.util.ArrayList");
+      list = arrayListClass()
+      list.add("pycarbon")
+      list.add(str(i))
+      list.add(str(i * 10))
+      list.add(base64.b64encode(content))
+      list.add(base64.b64encode(png_data))
+
+      writer.write(list.toArray())
+
+    writer.close()
+
+    reader = CarbonReader() \
+      .builder() \
+      .withFolder(path) \
+      .withBatch(1000) \
+      .build()
+
+    i = 0
+    while reader.hasNext():
+      rows = reader.readNextBatchRow()
+
+      for row in rows:
+        i = i + 1
+        print()
+        if 1 == i % 10:
+          print(str(i) + " to " + str(i + 9) + ":")
+        num = 0
+        for column in row:
+          num = num + 1
+          print(column, end="\t")
+          from jnius.jnius import ByteArray
+          if isinstance(column, ByteArray) and len(column) > 1000:
+            with open(path + "/image" + str(i) + "_" + str(num) + ".jpg", 'wb+') as file_object:
+              file_object.write(base64.b64decode(column.tostring()))
+
+    print()
+    print("number of rows by read: " + str(i))
+    assert 3 == i
+    reader.close()
+
+    end = time.time()
+    print("all time is " + str(end - start))
+    print("Finish")
